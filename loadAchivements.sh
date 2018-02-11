ef5dfb9232ab3fa7b81f64fc9f56bf232eb019c4
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-11 14:34:09 +0100

    #3: Using wget Script

--- loadAchivements.sh
+++ loadAchivements.sh
@@ -12,9 +12,10 @@ rm myGames.json
 # Load Achievments for every App
 while read appId; do
   logger "Load Achievments for AppId $appId"
-  wget --no-http-keep-alive --output-document="$appId.json" -q "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID"
-  #./wgetRetry.sh "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID" "$appId.json"
-  # Check if file is empty (typical when the app has no stats)
+  #wget --no-http-keep-alive --output-document="$appId.json" -q "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID"
+  ./wgetRetry.sh "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID" "$appId.json"
+  
+# Check if file is empty (typical when the app has no stats)
   fileSize=`wc -c < "$appId.json"`
   if [ $fileSize -eq 0 ]; then
      logger "Remove $appId.json because it's empty"
