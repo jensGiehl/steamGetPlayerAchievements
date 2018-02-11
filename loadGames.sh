bf813721df91a23c861021db5093827bce327d41
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-11 10:24:28 +0100

    Filename is now parameterized

--- loadGames.sh
+++ loadGames.sh
@@ -12,6 +12,6 @@ fi
 
 
 currentDate=`date '+%Y-%m-%d_%H%M'` 
-fileName="$STEAM_ID-GAMES-$currentDate"
+fileName=${1:-"$STEAM_ID-GAMES-$currentDate"}
 #wget -q "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_KEY&steamid=$STEAM_ID&format=json&include_appinfo=1&include_played_free_games=1" -O $fileName.json
 ./wgetRetry.sh "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_KEY&steamid=$STEAM_ID&format=json&include_appinfo=1&include_played_free_games=1" $fileName.json
