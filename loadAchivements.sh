3897a5e582e2a80dccce487fd18e5a5ecd82568b
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-11 10:43:45 +0100

    Extract AppIds first

--- loadAchivements.sh
+++ loadAchivements.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Load Gamelist
 ./loadGames.sh myGames
 
-while read p; do
-  echo $p
-done < myGames.json | grep "appid" | cut -d':' -f2 | sed -e 's/ //g' | sed -e 's/,//g'
+# Extract AppIds
+cat myGames.json | grep "appid" | cut -d':' -f2 | sed -e 's/ //g' | sed -e 's/,//g' > myAppIds.txt
+
+# Remove Gamelist
+rm myGames.json
+
+# Load Achievments for every App
+while read appId; do
+  logger "Load Achievments for AppId $appId"
+  ./wgetRetry.sh "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID" "$appId.json"
+done < myAppIds.txt
+
+# Remove AppIds
+rm myAppIds.txt
